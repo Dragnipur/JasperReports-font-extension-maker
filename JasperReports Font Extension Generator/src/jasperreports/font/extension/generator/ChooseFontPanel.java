0901b32bff168f1f9e802b8b1a8ade910736133e
Pieter van Dorst <ppj.vandorst@gmail.com>
2013-12-05 09:02:50 +0100

    Added the first implementation of the first GUI screen. (choosing the fonts)

--- JasperReports Font Extension Generator/src/jasperreports/font/extension/generator/ChooseFontPanel.java
+++ JasperReports Font Extension Generator/src/jasperreports/font/extension/generator/ChooseFontPanel.java
@@ -9,40 +9,82 @@ import java.awt.Cursor;
 import java.awt.dnd.DropTarget;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
 import javax.swing.JFileChooser;
+import javax.swing.JList;
+import javax.swing.filechooser.FileFilter;
 
 /**
  *
  * @author Pieter
  */
 public class ChooseFontPanel extends javax.swing.JPanel {
-
-    /**
-     * Creates new form guiPanel
-     */
+    
+    private JFileChooser fileChooser;
+    
     public ChooseFontPanel() {
         initComponents();
-
         FileDragDropListener dragDropListener = new FileDragDropListener();
-        DropTarget dropTarget = new DropTarget(FileDropPanel, dragDropListener);
-        FileDropPanel.addMouseListener(new FileUploadMouseListener());
+        DropTarget dropTarget = new DropTarget(fileDropPanel, dragDropListener);
+        fileDropPanel.addMouseListener(new FileUploadMouseListener());
+    }
 
+    private void handleFileInput(int result) {
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File[] files = fileChooser.getSelectedFiles();
+            
+            for(int i = 0; i < files.length; i++) {
+                String filePath = files[i].getAbsolutePath();
+            }
+        }
     }
 
     class FileUploadMouseListener implements MouseListener {
 
         @Override
         public void mouseClicked(MouseEvent e) {
-            final JFileChooser fc = new JFileChooser();
-            int returnVal = fc.showOpenDialog(FileDropPanel);
+
+            FileFilter fileFilter = new FileFilter() {
+                @Override
+                public boolean accept(File file) {
+
+                    if (file.isDirectory()) {
+                        return true;
+                    }
+
+                    String[] acceptedFileExtensions = {"otf", "TTF", "OTF", "ttf"};
+                    String fileName = file.getName();
+
+                    for (int i = 0; i < acceptedFileExtensions.length; i++) {
+                        String extension = acceptedFileExtensions[i];
+                        if (fileName.endsWith(extension) == true) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+
+                @Override
+                public String getDescription() {
+                    return "Open font files";
+                }
+            };
+
+            fileChooser = new JFileChooser();
+            fileChooser.setFileFilter(fileFilter);
+            fileChooser.setMultiSelectionEnabled(true);
+            int result = fileChooser.showOpenDialog(fileDropPanel);
+            handleFileInput(result);
         }
 
         @Override
         public void mousePressed(MouseEvent e) {
+            //do nothing
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
+            //do nothing
         }
 
         @Override
@@ -60,30 +102,53 @@ public class ChooseFontPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        dragAndDropPanel1 = new jasperreports.font.extension.generator.DragAndDropPanel();
-        FileDropPanel = new jasperreports.font.extension.generator.DragAndDropPanel();
+        fileDropPanel = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
 
         setPreferredSize(new java.awt.Dimension(700, 300));
+
+        fileDropPanel.setBackground(new java.awt.Color(255, 255, 204));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setText("Select your font files or drag them in!");
+
+        javax.swing.GroupLayout fileDropPanelLayout = new javax.swing.GroupLayout(fileDropPanel);
+        fileDropPanel.setLayout(fileDropPanelLayout);
+        fileDropPanelLayout.setHorizontalGroup(
+            fileDropPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, fileDropPanelLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(223, 223, 223))
+        );
+        fileDropPanelLayout.setVerticalGroup(
+            fileDropPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(fileDropPanelLayout.createSequentialGroup()
+                .addGap(113, 113, 113)
+                .addComponent(jLabel1)
+                .addContainerGap(132, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(89, 89, 89)
-                .addComponent(FileDropPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(104, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(89, Short.MAX_VALUE)
+                .addComponent(fileDropPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 431, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(81, 81, 81))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(25, 25, 25)
-                .addComponent(FileDropPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(51, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(fileDropPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(33, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private jasperreports.font.extension.generator.DragAndDropPanel FileDropPanel;
-    private jasperreports.font.extension.generator.DragAndDropPanel dragAndDropPanel1;
+    private javax.swing.JPanel fileDropPanel;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
